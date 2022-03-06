8096a5161d2438ce2996be78ca008ee240722837
Davidson jose <91620722+Davidsonjose@users.noreply.github.com>
2022-03-06 02:22:43 +0100

    express introduced.... talking more on form and routing

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      res.writeHead(200, { "Content-Type": "text/html" });
-      const homepagePath = path.join(__dirname, "./views/index.html");
-      const readHomePage = fs.readFileSync(homepagePath, "utf-8");
-      res.write(readHomePage);
-      res.end();
-      break;
-    case "/about":
-      const aboutPath = path.join(__dirname, "./views/about.html");
-      const readAboutPage = fs.readFileSync(aboutPath, "utf-8");
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(readAboutPage);
-      res.end();
-      break;
-    case "/contact":
-      const contactPath = path.join(__dirname, "./views/contact.html");
-      const readContactPath = fs.readFileSync(contactPath, "utf-8");
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(readContactPath);
-      res.end();
-      break;
-    default:
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(
-        "<h4>Sorry the route you requested does not exist or has been moved</h4>"
-      );
-      res.end();
-      break;
-  }
+const app = express();
+
+//body parser middleware for getting data from the front-end in json format
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+const homePath = path.join(__dirname, "./views/index.html");
+const readHomePath = fs.readFileSync(homePath, "utf-8");
+
+const aboutPath = path.join(__dirname, "./views/about.html");
+const readAboutPath = fs.readFileSync(aboutPath, "utf-8");
+
+const contactPath = path.join(__dirname, "./views/contact.html");
+const readContactPath = fs.readFileSync(contactPath, "utf-8");
+
+app.get("/", (req, res) => {
+  res.send(readHomePath);
+  // res.send("Welcome to the homepage")
+});
+app.get("/about", (req, res) => {
+  res.send(readAboutPath);
+  // res.send("Welcome to the homepage")
+});
+app.get("/contact", (req, res) => {
+  res.send(readContactPath);
+  // res.send("Welcome to the homepage")
 });
 
-const PORT = 5000;
-server.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}/`);
+
+app.post('/contact', (req, res)=>{
+    console.log(req.body);
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
